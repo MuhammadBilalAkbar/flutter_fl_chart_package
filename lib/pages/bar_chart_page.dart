eb4e9ca9fd5a5a1345d18cb4b0edb65a79ef1330
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-03-26 12:44:55 +0500

    line, bar and pie charts

--- lib/pages/bar_chart_page.dart
+++ lib/pages/bar_chart_page.dart
@@ -22,52 +22,55 @@ class BarChartPage extends StatelessWidget {
         title: const Text('Flutter Bar Chart'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: BarChart(
-          BarChartData(
-            borderData: FlBorderData(
-              border: const Border(
-                top: BorderSide.none,
-                right: BorderSide.none,
-                left: BorderSide(width: 1),
-                bottom: BorderSide(width: 1),
+        padding: const EdgeInsets.all(20),
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: BarChart(
+            BarChartData(
+              borderData: FlBorderData(
+                border: const Border(
+                  top: BorderSide.none,
+                  right: BorderSide.none,
+                  left: BorderSide(width: 1),
+                  bottom: BorderSide(width: 1),
+                ),
               ),
+              groupsSpace: 10,
+              barGroups: [
+                BarChartGroupData(x: 1, barRods: [
+                  BarChartRodData(
+                      fromY: 0, toY: 10, width: 15, color: Colors.amber),
+                ]),
+                BarChartGroupData(x: 2, barRods: [
+                  BarChartRodData(
+                      fromY: 0, toY: 10, width: 15, color: Colors.amber),
+                ]),
+                BarChartGroupData(x: 3, barRods: [
+                  BarChartRodData(
+                      fromY: 0, toY: 15, width: 15, color: Colors.amber),
+                ]),
+                BarChartGroupData(x: 4, barRods: [
+                  BarChartRodData(
+                      fromY: 0, toY: 10, width: 15, color: Colors.amber),
+                ]),
+                BarChartGroupData(x: 5, barRods: [
+                  BarChartRodData(
+                      fromY: 0, toY: 11, width: 15, color: Colors.amber),
+                ]),
+                BarChartGroupData(x: 6, barRods: [
+                  BarChartRodData(
+                      fromY: 0, toY: 10, width: 15, color: Colors.amber),
+                ]),
+                BarChartGroupData(x: 7, barRods: [
+                  BarChartRodData(
+                      fromY: 0, toY: 10, width: 15, color: Colors.amber),
+                ]),
+                BarChartGroupData(x: 8, barRods: [
+                  BarChartRodData(
+                      fromY: 0, toY: 10, width: 15, color: Colors.amber),
+                ]),
+              ],
             ),
-            groupsSpace: 10,
-            barGroups: [
-              BarChartGroupData(x: 1, barRods: [
-                BarChartRodData(
-                    fromY: 0, toY: 10, width: 15, color: Colors.amber),
-              ]),
-              BarChartGroupData(x: 2, barRods: [
-                BarChartRodData(
-                    fromY: 0, toY: 10, width: 15, color: Colors.amber),
-              ]),
-              BarChartGroupData(x: 3, barRods: [
-                BarChartRodData(
-                    fromY: 0, toY: 15, width: 15, color: Colors.amber),
-              ]),
-              BarChartGroupData(x: 4, barRods: [
-                BarChartRodData(
-                    fromY: 0, toY: 10, width: 15, color: Colors.amber),
-              ]),
-              BarChartGroupData(x: 5, barRods: [
-                BarChartRodData(
-                    fromY: 0, toY: 11, width: 15, color: Colors.amber),
-              ]),
-              BarChartGroupData(x: 6, barRods: [
-                BarChartRodData(
-                    fromY: 0, toY: 10, width: 15, color: Colors.amber),
-              ]),
-              BarChartGroupData(x: 7, barRods: [
-                BarChartRodData(
-                    fromY: 0, toY: 10, width: 15, color: Colors.amber),
-              ]),
-              BarChartGroupData(x: 8, barRods: [
-                BarChartRodData(
-                    fromY: 0, toY: 10, width: 15, color: Colors.amber),
-              ]),
-            ],
           ),
         ),
       ),
